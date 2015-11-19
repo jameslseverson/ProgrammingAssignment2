458b15b5af1592a539f76e721ad1267bedbf91be
jameslseverson <jameslseverson@gmail.com>
2015-11-18 22:10:48 -0600

    cachematrixinverse
    create function that creates a special matrix, and the quickly find
    inverse using cache when applicable

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## First Create Cached Matrix Object
 
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
@@ -18,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Now find inverse of matrix defined above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
